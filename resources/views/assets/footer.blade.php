3549938b30b7c017e05d989c9a293d4cf05b7ebc
Lưu Thanh Sơn <sonlam1102@hotmail.com>
2019-02-16 19:44:19 +0700

    Update footer

--- resources/views/assets/footer.blade.php
+++ resources/views/assets/footer.blade.php
@@ -1,4 +1,5 @@
 <div style='display:inherit' class='footer_div'>
-    <p>Created in: @2016</p>
+    <p>Vector Problem Solver Created in: @2016 and @2019 </p>
     <p>Authors: <b>Hien D. Nguyen, Pham M. Hoang, Dien V. Nguyen, Son T. Luu </b></p>
+    <p>Developed on <b>Feb 2019</b></p>
 </div>
