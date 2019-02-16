38e8124c630b9e05b5eb7c6bb20a85ee8c2d5b87
Lưu Thanh Sơn <sonlam1102@hotmail.com>
2019-02-16 11:57:59 +0700

    Update UI

---
+++ resources/views/input.blade.php
@@ -0,0 +1,86 @@
+<table cellspacing='0' cellpadding='0' width='100%' style='table-layout:fixed;width:100%; '>
+    <tr>
+        <td width='100%'>
+            <div id='keyboard_div' style="width:100%;">
+                <div class="row_desktop">
+                    <div id="math_cong" class="button">+</div>
+                    <div id="math_tru">&#8722;</div>
+                    <div id="math_bac2">
+                        <table style="width:100%;height:50%;border-spacing:0px;border-collapse:separate;margin-top:auto;margin-bottom:auto;">
+                            <tr>
+                                <td style="width:25%"></td>
+                                <td style="width:30%">
+                                    <div class="square_in_base"></div>
+                                </td>
+                                <td style="width:20%;padding:0px;margin:0px">
+                                    <div class="num_in_expt">
+                                        <div class="two_in_expt">2</div>
+                                    </div>
+                                </td>
+                                <td style="width:25%"></td>
+                            </tr>
+                        </table>
+                    </div>
+                    <div id="math_bacn">
+                        <table style="width:100%;height:50%;border-spacing:0px;border-collapse:separate;margin-top:auto;margin-bottom:auto;">
+                            <tr>
+                                <td style="width:25%"></td>
+                                <td style="width:30%">
+                                    <div class="square_in_base"></div>
+                                </td>
+                                <td style="width:20%">
+                                    <div class="num_in_expt">
+                                        <div class="square_in_expt"></div>
+                                    </div>
+                                </td>
+                                <td style="width:25%"></td>
+                            </tr>
+                        </table>
+                    </div>
+                    <div id="math_nhan">&#215;</div>
+                    <div id="math_vector">
+                        <table style="width:100%;height:50%;border-spacing:0px;border-collapse:separate;margin-top:auto;margin-bottom:auto;">
+                            <tr>
+                                <td style="width:25%"></td>
+                                <td style="width:30%">
+                                    <div class="square_in_base"></div>
+                                </td>
+                                <td style="width:20%;padding:0px;margin:0px">
+                                    <div class="num_in_arrow">
+                                        <div class="two_in_expt">&rarr;</div>
+                                    </div>
+                                </td>
+                                <td style="width:25%"></td>
+                            </tr>
+                        </table>
+                    </div>
+                    <div id="math_equal">=</div>
+                </div>
+
+                <div class="row_desktop">
+                    <div id="math_br_l">(</div>
+                    <div id="math_br_r">)</div>
+                    <div id="math_sqrt">&#8730;
+                        <div class="square_big"></div>
+                    </div>
+                    <div id="math_sqrtn"><sup>
+                            <div class="square_small"></div>
+                        </sup>&#8730;
+                        <div class="square_big"></div>
+                    </div>
+                    <div id="math_chia">&#247;</div>
+                    <div id="math_splash">/</div>
+                    <div id="math_frac">
+                        <div class="square_big_in_frac"></div>
+                        <div class="frac_line"></div>
+                        <div class="square_big_in_frac"></div>
+                    </div>
+                </div>
+            </div>
+        </td>
+        <td width='30%'>
+            <input id="step_solve" class='submit_solve' style='height:100%;line-height:40px; '
+                   type='submit' value='GỈAI!'/>
+        </td>
+    </tr>
+</table>
