61d25fd1051b6dedd8080964cc3adc2737e40ab0
Lưu Thanh Sơn <sonlam1102@hotmail.com>
2019-02-17 20:22:18 +0700

    Hide guiding page

--- resources/views/assets/header.blade.php
+++ resources/views/assets/header.blade.php
@@ -37,12 +37,6 @@
                             <div class='logo_text_math'>Đề bài mẫu</div>
                         </a>
                     </td>
-
-                    <td class='nar_bar_cell' align='left'>
-                        <a href='/guide'>
-                            <div class='logo_text_math'>Hướng dẫn </div>
-                        </a>
-                    </td>
                 </tr>
             </table>
         </td>
